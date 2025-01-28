4eb06e1dd7a84bc5d81ec4e27a57c6ad308ec788
George Vrakas <144050521+GeorgeVrk@users.noreply.github.com>
2025-01-28 18:40:53 +0200

    DEV : Adding stuff

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -14,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using Serilog;
+using AI.Test;
 
 namespace Snake
 {
@@ -26,7 +27,36 @@ namespace Snake
         [STAThread]
         static void Main(string[] args)
         {
-            GameHandler.StartGame();
+            ParseOptions(args);
+        }
+
+        public static void ParseOptions(string[] args)
+        {
+            var o = new Options();
+            try
+            {
+                o.Parse(args);
+                if (o.auto && o.manual)
+                {
+                    throw new ArgumentException("Invalid options: Choose either -a (auto) or -m (manual), not both.");
+                }
+                if (o.manual)
+                {
+                    GameHandler.StartGame();
+                }
+                else if (o.auto)
+                {
+                    QLearning.Start();
+                }
+                else
+                {
+                    throw new ArgumentException("No valid option selected. Use -a for auto or -m for manual.");
+                }
+            }
+            catch (Exception ex)
+            {
+                s_log.Error($"Error parsing options: {ex.Message}");
+            }
         }
 
     }
